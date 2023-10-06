46e5b3e41df018d41ffa3beb1cce163aaeef9266
victor braz <victor.90braz@gmail.com>
2023-10-06 17:02:46 +0200

    validate form

--- controllers/registration/store.php
+++ controllers/registration/store.php
@@ -7,12 +7,19 @@ $password = $_POST['password'];
 
 // validate the form inputs
 $errors = [];
+
 if (!Validator::email($email) ) {
-    $errors[] = "Please provide a valid email address";
+    $errors['email'] = "Please provide a valid email address.";
 }
 
 if (!Validator::string($password,7,15) ) {
-    $errors[] = "Please provide a valid password";
+    $errors['password'] = "Please provide a password at least seven characters.";
+}
+
+if (! empty($errors)) {
+    return view('registration/create.view.php', [
+        'errors' => $errors
+    ]);
 }
 
 // check if the account already exists
