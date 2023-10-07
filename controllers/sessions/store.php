d95ac50c5c0d6f4e2bc3bef28f985f76db5433a9
victor braz <victor.90braz@gmail.com>
2023-10-07 11:50:55 +0200

    updated routes with sessions name

---
+++ controllers/sessions/store.php
@@ -0,0 +1,3 @@
+<?php
+
+dd('submited the form');
--- routes.php
+++ routes.php
@@ -18,5 +18,5 @@ $router->get('/register', 'controllers/registration/create.php')->only('guest');
 $router->post('/register', 'controllers/registration/store.php');
 
 $router->get('/login', 'controllers/sessions/create.php')->only('guest');
-$router->post('/login', 'controllers/sessions/store.php')->only('guest');
+$router->post('/sessions', 'controllers/sessions/store.php')->only('guest');
 
