85828874a9cdc8cf7c1930a5f1dfa0348396ee8a
victor braz <victor.90braz@gmail.com>
2023-10-07 12:53:04 +0200

    set new layout to forms

--- views/sessions/create.view.php
+++ views/sessions/create.view.php
@@ -14,10 +14,6 @@
           <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
           <div class="mt-2">
             <input id="email" name="email" type="email" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-
-            <?php if (isset($errors['email'])) : ?>
-              <p class="text-red-500 text-xs mt-2"><?= $errors['email'] ?></p>
-            <?php endif; ?>
           </div>
         </div>
 
@@ -37,6 +33,10 @@
         <div>
           <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Log In</button>
         </div>
+
+        <?php if (isset($errors['email'])) : ?>
+              <p class="text-red-500 text-xs mt-2"><?= $errors['email'] ?></p>
+            <?php endif; ?>
       </form>
     </div>
   </div>
